e2c8d4b3d57831fd27baa3f250f7eb6d0105b100
Anders Bech Mellson <anders@bechmellson.com>
2012-04-28 17:00:27 +0200

    Got the access to the document working

--- src/mirror/MirrorView.scala
+++ src/mirror/MirrorView.scala
@@ -6,27 +6,38 @@ import org.eclipse.swt.SWT
 import org.eclipse.ui.part.ViewPart
 import org.eclipse.ui.IEditorPart
 import org.eclipse.ui.PlatformUI
+import java.beans.PropertyChangeListener
+import org.eclipse.ui.IPropertyListener
+import org.eclipse.swt.events.KeyListener
+import org.eclipse.swt.events.KeyEvent
+import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor
+import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor
+import org.eclipse.swt.widgets.Control
+import org.eclipse.jface.text.IDocument
+import org.eclipse.ui.texteditor.ITextEditor
+import org.eclipse.jface.text.IDocumentListener
+import org.eclipse.jface.text.DocumentEvent
 
 class MirrorView extends ViewPart {
 	val ID = "mirror.views.MirrorView"
 	var text: StyledText = null
 	var activeEditor: IEditorPart = null
-	
 	def createPartControl(parent: Composite): Unit = {
-	  text = new StyledText(parent, SWT.BORDER)
-	  text setEditable false
-	  
-	  activeEditor = PlatformUI.getWorkbench.getActiveWorkbenchWindow.getActivePage.getActiveEditor
+		text = new StyledText(parent, SWT.BORDER)
+		text setEditable false
+		activeEditor = PlatformUI.getWorkbench.getActiveWorkbenchWindow.getActivePage.getActiveEditor
+		if (activeEditor != null) {
+			val document: IDocument = activeEditor.asInstanceOf[ITextEditor].getDocumentProvider.getDocument(activeEditor.getEditorInput)
+			document.addDocumentListener(new IDocumentListener {
+				def documentAboutToBeChanged(event:DocumentEvent) {}
+				def documentChanged(event:DocumentEvent) {
+					text setText document.get
+					}
+				})
+			}
+		}
 
-	  if (activeEditor != null)
-		  text setText activeEditor.getEditorInput.getName
-	  else
-		  text setText "Null - intet her"
-	}
-
-	def setFocus(): Unit = {
-	  text setText (text getText) + "."
-	}
+	def setFocus(): Unit = { }
 }
 
 object MirrorView extends MirrorView { }
