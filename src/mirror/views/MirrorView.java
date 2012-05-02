d9f2f52f583e3845bd1feb10880af436e2ac2d11
Anders Bech Mellson <anders@bechmellson.com>
2012-05-02 20:54:46 +0200

    Fix conflicts

--- src/mirror/views/MirrorView.java
+++ src/mirror/views/MirrorView.java
@@ -3,15 +3,18 @@ package mirror.views;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
 public class MirrorView extends ViewPart {
-
+	
 	/**
 	 * The ID of the view as specified by the extension.
 	 */
 	public static final String ID = "mirror.views.MirrorView";
 	private StyledText text;
+	private IEditorPart activeEditor;
 
 	/**
 	 * The constructor.
@@ -26,20 +29,22 @@ public class MirrorView extends ViewPart {
 	public void createPartControl(Composite parent) {
 		text = new StyledText(parent, SWT.BORDER);
 		text.setEditable(false);
-
-		text.setText("Anders");
+		
+		activeEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+				.getActiveEditor();
+		text.setText(activeEditor.getEditorInput().getName());
 	}
-		/**
-		 * Passing the focus request to the viewer's control.
-		 */
-		public void setFocus() {
-			text.setFocus();
-			System.out.println("focus");
-			text.setText(text.getText()+".");
-		}
 
-//		public void dispose() {
-//			super.dispose();
-//		}
-
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	public void setFocus() {
+		//text.setFocus();
+		//text.setText(activeEditor.getEditorInput().getName());
 	}
+
+	public void dispose() {
+		super.dispose();
+	}
+
+}
